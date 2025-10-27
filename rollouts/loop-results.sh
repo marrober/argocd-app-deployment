90f0ca7cbf167ffb812fe91b5fc2e1fa1b80b45a
Mark Roberts <marrober@redhat.com>
2025-10-27 15:01:03 +0000

    update

--- rollouts/loop-results.sh
+++ rollouts/loop-results.sh
@@ -1,1 +1,1 @@
-for i in {1..300}; do curl http://layer-1-route-dev-rollout-01.apps.cluster-4nggh.4nggh.sandbox5381.opentlc.com/get-results; echo "------------------------------------------------------";sleep 10; done
+for i in {1..300}; do curl http://layer-1-route-rollouts.apps.ocp4.mr-openshift.co.uk/get-results; echo "------------------------------------------------------";sleep 10; done
